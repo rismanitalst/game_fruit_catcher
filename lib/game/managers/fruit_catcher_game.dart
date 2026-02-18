46bbb567957b9b443022310d03ae629e2940ad02
rismanitalst <rismanitalst@gmail.com>
2026-02-19 04:19:58 +0700

    feat: create FruitCatcherGame sebagai base FlameGame

---
+++ lib/game/managers/fruit_catcher_game.dart
@@ -0,0 +1,9 @@
+import 'package:flame/game.dart';
+import 'package:flutter/material.dart';
+
+class FruitCatcherGame extends FlameGame {
+  @override
+  Color backgroundColor() {
+    return const Color(0xFF87CEEB);
+  }
+}
