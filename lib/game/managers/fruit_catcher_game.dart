9261fd485c733d2ff0334ef8aeb2446ab42526b1
rismanitalst <rismanitalst@gmail.com>
2026-02-19 20:22:41 +0700

    feat: add fruit spawning system dengan TimerComponent

--- lib/game/managers/fruit_catcher_game.dart
+++ lib/game/managers/fruit_catcher_game.dart
@@ -1,10 +1,15 @@
+import 'dart:math';
+
+import 'package:flame/components.dart';
 import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
 
 import '../components/basket.dart';
+import '../components/fruit.dart';
 
-class FruitCatcherGame extends FlameGame
-    with HasCollisionDetection {
+class FruitCatcherGame extends FlameGame {
+
+  final Random random = Random();
 
   int score = 0;
 
@@ -15,14 +20,34 @@ class FruitCatcherGame extends FlameGame
   Future<void> onLoad() async {
     await super.onLoad();
 
+    // spawn basket
     final basket = Basket(
       position: Vector2(size.x / 2, size.y - 100),
     );
 
     add(basket);
+
+    // timer spawn fruit
+    add(
+      TimerComponent(
+        period: 1.5,
+        repeat: true,
+        onTick: spawnFruit,
+      ),
+    );
   }
 
-  //dipanggil Fruit saat collision
+  void spawnFruit() {
+    final xPosition = random.nextDouble() * size.x;
+
+    final fruit = Fruit(
+      position: Vector2(xPosition, -30),
+      type: Fruit.randomType(),
+    );
+
+    add(fruit);
+  }
+
   void incrementScore() {
     score++;
     debugPrint('Score: $score');
