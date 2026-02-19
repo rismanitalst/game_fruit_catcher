ec1ff848d4175622e8f0e6523d00fa9fc69b8765
rismanitalst <rismanitalst@gmail.com>
2026-02-19 19:58:30 +0700

    update: basket.dart

--- lib/game/components/basket.dart
+++ lib/game/components/basket.dart
@@ -1,9 +1,10 @@
 import 'package:flame/components.dart';
-import 'package:flutter/material.dart';
 import 'package:flame/collisions.dart';
+import 'package:flutter/material.dart';
 
+class Basket extends PositionComponent
+    with HasGameRef, CollisionCallbacks {
 
-class Basket extends PositionComponent {
   Basket({required Vector2 position}) {
     this.position = position;
     size = Vector2(120, 40);
@@ -12,7 +13,7 @@ class Basket extends PositionComponent {
 
   @override
   Future<void> onLoad() async {
-    super.onLoad();
+    await super.onLoad();
 
     add(
       RectangleHitbox()
